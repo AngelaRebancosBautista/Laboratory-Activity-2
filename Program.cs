0cd178481dfd4ffcabe2dd2bf833f7382207909e
AngelaRebancosBautista <angelarbautista22@gmail.com>
2025-07-25 12:23:58 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory_Activity_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            int ageInMonths;
+            int age;
+            Console.Write("Enter your age in years (1 to 120): ");
+            age = int.Parse(Console.ReadLine());
+
+
+            if (age >= 1 && age <= 120)
+            {
+                ageInMonths = age * 12;
+                Console.WriteLine($"You are {ageInMonths} months old.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Age must be between 1 and 120.");
+            }
+        }
+    }
+}
+
+
